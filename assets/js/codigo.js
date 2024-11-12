a94e13f66cc11dde6e7641c83707ad1e53eb77ce
LuquitaDoidao <lucasamarantelima7@gmail.com>
2024-11-11 21:09:53 -0300

    aaa

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -1,8 +1,5 @@
 function mostrarHistorico() {
     const historico = document.getElementById("historico");
-    if (historico.style.display === "none" || historico.style.display === "") {
-        historico.style.display = "block";
-    } else {
-        historico.style.display = "none";
-    }
+    historico.style.display = historico.style.display === "none" || historico.style.display === "" ? "block" : "none";
 }
+
